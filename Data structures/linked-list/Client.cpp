46fac5fe7598f39cf3b29cad0ad7493460b1b26c
Diego Jimenez <diegojmoravia@gmail.com>
2023-04-18 19:16:42 -0600

    Observer added

---
+++ Data structures/linked-list/Client.cpp
@@ -0,0 +1,5 @@
+//
+// Created by diego on 12/4/2023.
+//
+
+#include "Client.h"
---
+++ Data structures/linked-list/Client.h
@@ -0,0 +1,14 @@
+//
+// Created by diego on 12/4/2023.
+//
+
+#ifndef LINKED_LIST_CLIENT_H
+#define LINKED_LIST_CLIENT_H
+
+
+class Client {
+
+};
+
+
+#endif //LINKED_LIST_CLIENT_H
